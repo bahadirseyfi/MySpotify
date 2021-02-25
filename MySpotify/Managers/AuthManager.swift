5160bba1b818d1ecaa732e106188f47b64391eaf
bahadirseyfi <bahadirsoftware@gmail.com>
2021-02-25 23:58:17 +0300

    Auth Module Part - 1
    Compated Functions: exchangeCodeForToken, signInURL, Constants, AuthViewController( didStartProvisionalNavigation, token)

--- MySpotify/Managers/AuthManager.swift
+++ MySpotify/Managers/AuthManager.swift
@@ -10,7 +10,19 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "205d740ce0994e81a6cd988ca26ea508"
+        static let clientSecret = "ce7ec8415bd64b2bae575b213e0be0e7"
+    }
     private init(){}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://github.com/bahadirseyfi/MySpotify"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSsignedIn: Bool {
         return false
@@ -30,5 +42,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    public func exchangeCodeForToken(
+        code: String,
+        completion: @escaping ((Bool) -> Void)
+    ) {
+        // Get Token
+    }
+    public func refresAccesshToken(){
+        
+    }
+    private func cacheToken(){
+        
     }
 }
